8e9f49616afed46a73e23847e327af1be28ddabc
Moula Bee <140152134+moulabi1911@users.noreply.github.com>
2023-09-30 19:40:45 +0530

    Update ma.tf

---
+++ ma.tf
@@ -0,0 +1,17 @@
+#Creating EC2 instances on AWS cloud using Terraform
+
+provider "aws" {
+region = "ap-southeast-2"
+}
+
+resource "aws_instance" "one" {
+ami = "ami-0373aa64534d82bf6"
+instance_type = "t2.micro"
+}
+
+
+
+#terraform init
+#terraform plan
+#terraform apply --auto-approve
+#terraform destroy --auto-approve
